e5873af856d9c545da3f0ab8e5c5d469e62ce447
OptiNex <168401522+Official-OptiNex@users.noreply.github.com>
2024-10-20 17:11:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -204,8 +204,8 @@ async function updateEmbed(channel, messageId, totalPrize) {
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     console.log(`${client.user.tag} Is now checking for BloxFlip rain events every 5 sconds, and updating any rain embeds every 2 seconds`);
-    // Run the checkRain function every 5 seconds
-    setInterval(checkRain, 5 * 1000);
+    // Run the checkRain function every 7 seconds
+    setInterval(checkRain, 7 * 1000);
     // Run once on start
     checkRain();
 });
